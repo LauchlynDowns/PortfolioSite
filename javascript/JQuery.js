b43a0687a513409a89769dbc0d9356e4cf936e6e
LauchlynDowns <49620443+LauchlynDowns@users.noreply.github.com>
2021-12-29 23:12:04 +0000

    added smooth scrolling on project selector

--- javascript/JQuery.js
+++ javascript/JQuery.js
@@ -1,4 +1,16 @@
 
+//vanilla JS used to make buttons scroll on gallery.
+const leftbutton = document.getElementById('left');
+const rightbutton = document.getElementById('right');
+//find document width and times it by 0.2 to get the width of 20vw
+const ScrollWidth = document.body.clientWidth * 0.2;
+rightbutton.addEventListener("click", function () { document.getElementById('project-selector').scrollLeft += ScrollWidth; })
+leftbutton.addEventListener("click", function () { document.getElementById('project-selector').scrollLeft += -ScrollWidth })
+
+
+
+
+
 //code for home-page animation
 $("document").ready(function () {
     $(".navbar-text, .introtext, .social-links, #codeshape").hide();
@@ -10,37 +22,34 @@ $("document").ready(function () {
 
 
 
-//code from https://github.com/johnboker/jquery.blink
-(function ($) {
-    $.fn.blink = function (options) {
-        var defaults = { delay: 500 };
-        var options = $.extend(defaults, options);
-        return $(this).each(function (idx, itm) {
-            var handle = setInterval(function () {
-                if ($(itm).css("visibility") === "visible") {
-                    $(itm).css('visibility', 'hidden');
-                } else {
-                    $(itm).css('visibility', 'visible');
+    //code from https://github.com/johnboker/jquery.blink
+    (function ($) {
+        $.fn.blink = function (options) {
+            var defaults = { delay: 500 };
+            var options = $.extend(defaults, options);
+            return $(this).each(function (idx, itm) {
+                var handle = setInterval(function () {
+                    if ($(itm).css("visibility") === "visible") {
+                        $(itm).css('visibility', 'hidden');
+                    } else {
+                        $(itm).css('visibility', 'visible');
+                    }
+                }, options.delay);
+
+                $(itm).data('handle', handle);
+            });
+        }
+        $.fn.unblink = function () {
+            return $(this).each(function (idx, itm) {
+                var handle = $(itm).data('handle');
+                if (handle) {
+                    clearInterval(handle);
+                    $(itm).data('handle', null);
+                    $(itm).css('visibility', 'inherit');
                 }
-            }, options.delay);
-
-            $(itm).data('handle', handle);
-        });
-    }
-    $.fn.unblink = function () {
-        return $(this).each(function (idx, itm) {
-            var handle = $(itm).data('handle');
-            if (handle) {
-                clearInterval(handle);
-                $(itm).data('handle', null);
-                $(itm).css('visibility', 'inherit');
-            }
-        });
-    }
-}(jQuery))
-
-
-
+            });
+        }
+    }(jQuery))
 
 
 
